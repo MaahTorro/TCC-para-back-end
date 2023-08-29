7f052b483aeb6ce4dd20a89a5192f34c34290fb0
Marcela Torro <99346289+MaahTorro@users.noreply.github.com>
2023-08-29 13:25:31 -0300

    Add files via upload

---
+++ src/app/listaprod/listaprod.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listaprod',
+  templateUrl: './listaprod.page.html',
+  styleUrls: ['./listaprod.page.scss'],
+})
+export class ListaprodPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
